a0f7660632a3650868a0ce012c4b441883a96936
Sebastian <smcasanova17@hotmail.com>
2022-10-26 21:51:25 -0400

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,25 @@
+var currentDate =moment().format("MMMM Do YYYY, hA")
+$("#currentDay").text(currentDate);
+
+// if hour is equal too timeblock make .present css value
+    //function(){
+    //    if 
+    //}
+// if hour is less than the index make .past
+// if hour is greater than the index make .future
+// create ability for user to input text
+// create save button for text
+// use moment().format(hA) to make dynamic color editor
+
+// user types paragraph into paragraph box
+
+let hour9 = document.getElementById("9hour")
+// user clicks save button
+window.addEventListener('click',
+)
+// save user text button
+// figure out where to text from
+
+
+// get text from area
+// save too local storage
