07b4231262df889ca4be5e8fd5dba5bceffd7f0d
Sebastian <smcasanova17@hotmail.com>
2022-10-29 08:00:19 -0400

    created save button

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,3 @@
-var bullShitEl = document.getElementById('#bullShit')
  // contains current date to the hour
 var currentDate = moment().format("MMMM Do YYYY, hA");
 
@@ -7,15 +6,34 @@ let allSaveBtns = $('.saveBtn')
 
 
 $('.saveBtn').click(saveButton)
+function saveButton(){
+    let btnTxt = $(this).siblings('.col-md-10').val()
+    let hourIndicator = $(this).parent().attr('id')
 
-$("#hour9 .description").val(localStorage.getItem("hour9"));
+    localStorage.setItem(hourIndicator,btnTxt)
+}
 
+$("#hour-9 .col-md-10").val(localStorage.getItem("hour-9"));
+$("#hour-10 .col-md-10").val(localStorage.getItem("hour-10"));
+$("#hour-11 .col-md-10").val(localStorage.getItem("hour-11"));
+$("#hour-12 .col-md-10").val(localStorage.getItem("hour-12"));
+$("#hour-1 .col-md-10").val(localStorage.getItem("hour-1"));
+$("#hour-2 .col-md-10").val(localStorage.getItem("hour-2"));
+$("#hour-3 .col-md-10").val(localStorage.getItem("hour-3"));
+$("#hour-4 .col-md-10").val(localStorage.getItem("hour-4"));
+$("#hour-5 .col-md-10").val(localStorage.getItem("hour-5"));
+$("#hour-6 .col-md-10").val(localStorage.getItem("hour-6"));
 
-function saveButton(event){
-        let btnTxt = $(event.target).siblings('textarea')
-        localStorage.setItem(btnTxt.attr('id'),btnTxt.val())
-        let savedStuff = (localStorage.getItem(btnTxt.attr('id')))
-        bullShitEl.innerText = "asshole"
+var currentHour = moment().hour()
+var hour = 
+
+function hourColorSetter(){
+    if (hour == currentHour){
+        setPresentColor
+    }else if(hour < currentHour){
+        setPastColor
+    }else{
+        setFutureColor
     }
-    
+}
     
